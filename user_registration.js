40cfd293d9163a5735429736afc4fdc8544ebdcb
Piyush Kadam <kadam.piyush.17it1041@gmail.com>
2021-08-11 21:51:05 +0530

    UC2_valid_lastname

--- user_registration.js
+++ user_registration.js
@@ -1,6 +1,5 @@
 //uc1 user first name cap with min. 3 characters
-function validation(u_name) {
-    var regx = /^[A-Z]([\w]{3})([\w]+)?$/;
+function validation(uname,regx) {
     if (regx.test(uname) == false) {
         throw 'Not Valid';
     }
@@ -9,10 +8,21 @@ function validation(u_name) {
     }
 }
 
-var uname = "Pk1";
-console.log(uname);
+var firstname = "Pk1";
+var regx = /^[A-Z]([\w]{3})([\w]+)?$/;
 try {
-    validation(uname);
+    console.log(firstname);
+    validation(firstname,regx);
+    }
+catch(e) {
+    console.error(e);
+}
+
+//uc2_valid_lastname user last name start with cap with min. 3 characters
+var lastname = "Mehta";  
+try {
+    console.log(lastname);
+    validation(lastname,regx);
     }
 catch(e) {
     console.error(e);
